12e921da97d3e66f627938cd4c75935b46993c5e
andrea8980 <andrea@casaloda.it>
2026-07-06 17:23:40 +0200

    Refactor Details component to Home; add Bootstrap card layout for content display

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,52 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-export default function Details() {
+export default function Home() {
 
     return (
         <>
-        <h1>Details</h1>
+            <div className="row row-cols-2 g-1">
+                <div className="card my-4 mx-auto" style={{ minWidth: '18rem' }}>
+                    <img src="https://placedog.net/280/400" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                    </div>
+                </div>
+
+                <div className="recensioni d-flex flex-column align-items-center justify-content-start gap-3 mt-5">
+
+                    <h2 className="reviews-title-section">BananaReviews dei nostri recensori:</h2>
+
+                    <div className="card" style={{ minWidth: '18rem' }}>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="card-link">Card link</a>
+                            <a href="#" className="card-link">Another link</a>
+                        </div>
+                    </div>
+                    
+                    <div className="card" style={{ minWidth: '18rem' }}>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <a href="#" className="card-link">Card link</a>
+                            <a href="#" className="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
